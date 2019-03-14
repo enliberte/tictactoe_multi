513566eb5a4dfd76e8c403efc2f70042095c8a7a
enliberte <zugife21@gmail.com>
2019-03-14 23:23:12 +0300

    'first'

---
+++ client/js/client.js
@@ -0,0 +1,97 @@
+const sock = io();
+
+
+function borderCell(event) {
+    if (event.target.classList.contains('cell')) {
+        if (event.type === 'mouseover') {
+            event.target.style.border = '3px green solid';
+        } else if (event.type === 'mouseout') {
+            event.target.style.border = '1px white solid';
+        }
+    }
+}
+
+
+function sendCell(event) {
+    if (event.target.classList.contains('cell')) {
+        sock.emit('pressedCell',
+            {
+                id: event.target.id,
+                row: event.target.getAttribute('row'),
+                cln: event.target.getAttribute('cln')
+            });
+    }
+}
+
+
+function startNewGame(event) {
+    let gameResult = document.querySelector('#result');
+    document.body.removeChild(gameResult);
+    sock.emit('newGame');
+}
+
+
+class GameField {
+    constructor(size) {
+        this.size = size;
+        this.field = document.querySelector('#field');
+        sock.on('gameState', (state) => {
+            console.log(state);
+            this.refresh(state.cells);
+            this.showResult(state.winner);
+        });
+    }
+
+    showResult(result) {
+        if (result) {
+            let gameResult = document.createElement('div');
+            gameResult.id = 'result';
+            if (result === 'Ничья') {
+                gameResult.textContent = result;
+            } else {
+                gameResult.textContent = `Выиграли: ${result}`
+            }
+            gameResult.style.left = `${this.field.clientHeight / 2 - gameResult.clientWidth / 2}px`;
+            gameResult.style.top = `-${this.field.clientHeight / 2 + gameResult.clientHeight / 2}px`;
+            document.body.appendChild(gameResult);
+        }
+    }
+
+    refresh(cells) {
+        for (let cell in cells) {
+            if (cells[cell]) {
+                document.querySelector(`#${cell}`).textContent = cells[cell];
+            } else {
+                document.querySelector(`#${cell}`).textContent = '';
+            }
+        }
+
+    }
+
+    draw() {
+        this.field.addEventListener('click', sendCell);
+        this.field.addEventListener('mouseover', borderCell);
+        this.field.addEventListener('mouseout', borderCell);
+        for (let rowNum = 0; rowNum < this.size; rowNum++) {
+            let row = document.createElement('div');
+            row.classList.add('line');
+            this.field.appendChild(row);
+            for (let cellNum = 0; cellNum < this.size; cellNum++) {
+                let cell = document.createElement('div');
+                cell.classList.add('cell');
+                cell.id = `r${rowNum}c${cellNum}`;
+                cell.setAttribute('row', `${rowNum}`);
+                cell.setAttribute('cln', `${cellNum}`);
+                let cellSize = document.documentElement.clientHeight / this.size;
+                cell.style.height = `${cellSize}px`;
+                cell.style.width = `${cellSize}px`;
+                cell.style.lineHeight = `${cellSize}px`;
+                cell.style.fontSize = `${cellSize * 0.8}px`;
+                row.appendChild(cell);
+            }
+        }
+    }
+}
+
+new GameField(20).draw();
+document.querySelector('#refresh').addEventListener('click', startNewGame);
---
+++ server.js
@@ -0,0 +1,125 @@
+const http = require('http');
+const express = require('express');
+const socketio = require('socket.io');
+
+
+class Game {
+    constructor(size) {
+        this.size = size;
+        this.toWin = 5;
+    }
+
+    getFigure() {
+        return (this.currentFigure) ? 'X' : 'O';
+    }
+
+    setState(cell) {
+        if (!this.field[cell.id]) {
+            this.field[cell.id] = this.getFigure();
+            return true;
+        }
+        return false;
+    }
+
+    calcSum(cell, currentSum) {
+        let sum = currentSum;
+        if (cell) {
+            if (cell === this.getFigure()) {
+                sum++;
+            } else {
+                sum = 0;
+            }
+        }
+        return sum;
+    }
+
+    checkWinner(cell) {
+        let sumInRow = 0;
+        let sumInCln = 0;
+        let sumInLR = 0;
+        let sumInRL = 0;
+
+        //ничья
+        if (Object.values(this.field).every((cellState) => {return !!cellState})) {
+            this.winner = 'Ничья';
+            return;
+        }
+
+        //выигрыш
+        for (let offset = -this.toWin + 1; offset < this.toWin; offset++) {
+            sumInRow = this.calcSum(this.field[`r${cell.row}c${+cell.cln + offset}`], sumInRow); //ряд
+            sumInCln = this.calcSum(this.field[`r${+cell.row + offset}c${cell.cln}`], sumInCln); //колонка
+            sumInRL = this.calcSum(this.field[`r${+cell.row + offset}c${+cell.cln + offset}`], sumInRL); //диагональ справа-налево
+            sumInLR = this.calcSum(this.field[`r${+cell.row - offset}c${+cell.cln + offset}`], sumInLR); //диагональ слева-направо
+            if (sumInRow === this.toWin || sumInCln === this.toWin || sumInRL === this.toWin || sumInLR === this.toWin) {
+                this.winner = this.getFigure();
+                return;
+            }
+        }
+    }
+
+    initField(){
+        this.currentFigure = true;
+        this.winner = null;
+        this.field = this.createFieldObj();
+    }
+
+    createFieldObj() {
+        let fieldObj = {};
+        for (let rowNum = 0; rowNum < this.size; rowNum++) {
+            for (let clnNum = 0; clnNum < this.size; clnNum++) {
+                fieldObj[`r${rowNum}c${clnNum}`] = null;
+            }
+        }
+        return fieldObj;
+    }
+
+
+}
+
+let game = new Game(20);
+game.initField();
+
+const port = process.env.PORT || 8000;
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+
+//отдача статики
+app.use(express.static(`${__dirname}/client`));
+
+
+io.on('connection', (sock) => {
+    sock.emit('gameState',
+        {
+            cells: game.field,
+            winner: game.winner
+        });
+    sock.on('pressedCell', (cell) => {
+        if (game.setState(cell)) {
+            game.checkWinner(cell);
+            game.currentFigure = !game.currentFigure;
+        }
+        io.emit('gameState',
+            {
+                cells: game.field,
+                winner: game.winner
+            });
+    });
+    sock.on('newGame', () => {
+        game.initField();
+        io.emit('gameState',
+            {
+                cells: game.field,
+                winner: game.winner
+            });
+    });
+});
+
+
+
+server.on('error', (err) => {console.log(`Server error: ${err}`)});
+server.listen(port, () => {console.log(`App is running on port ${port}`);});
+
+
